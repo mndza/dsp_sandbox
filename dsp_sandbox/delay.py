c382115cf258fb646c075ed6c438dd4d7aec24c8
mndza <diego.hdmp@gmail.com>
2023-05-08 16:59:56 +0200

    StreamDelay: add memory backend

--- dsp_sandbox/delay.py
+++ dsp_sandbox/delay.py
@@ -1,29 +1,61 @@
 from amaranth import *
+from amaranth.lib.fifo import SyncFIFO
 from .streams import SampleStream
 
-# TODO: add option for Memory-backed Delay
-
 class StreamDelay(Elaboratable):
-    def __init__(self, shape, delay):
-        self.delay        = delay
+    def __init__(self, shape, delay, storage="auto"):
+        self.depth        = delay
         self.shape        = shape
+        self.storage      = storage
         self.input        = SampleStream(shape)
         self.output       = SampleStream(shape)
 
     def elaborate(self, platform):
+        storage = self.storage
+        if storage == "auto":
+            # Decide to use FFs or RAM depending on depth
+            storage = "bram" if self.depth > 2 else "distributed"
+
+        if storage == "distributed":
+            return self.elaborate_distributed(platform)
+        elif storage == "bram":
+            return self.elaborate_bram(platform)
+        
+    def elaborate_bram(self, platform):
         m = Module()
 
-        delay_line = [ StreamRegister(self.shape) for _ in range(self.delay) ]
-        m.submodules += delay_line
+        w_data = self.input.payload
+        r_data = self.output.payload
+
+        m.submodules.fifo = fifo = SyncFIFO(width=len(w_data), depth=self.depth, fwft=False)
+        m.d.comb += [
+            # Write signaling
+            fifo.w_data      .eq(w_data),
+            fifo.w_en        .eq(self.input.valid),
+            self.input.ready .eq(fifo.w_rdy),
+            # Read signaling
+            fifo.r_en        .eq(self.output.produce),
+            r_data           .eq(fifo.r_data),
+        ]
+        with m.If(fifo.r_en):
+            m.d.sync += self.output.valid.eq(fifo.r_rdy)
+
+        return m
+
+    def elaborate_distributed(self, platform):
+        m = Module()
+
+        reg_line = [ StreamRegister(self.shape) for _ in range(self.depth) ]
+        m.submodules += reg_line
 
         last = self.input
-        for delay in delay_line:
-            m.d.comb += delay.input.stream_eq(last)
-            last = delay.output
+        for reg in reg_line:
+            m.d.comb += reg.input.stream_eq(last)
+            last = reg.output
         m.d.comb += self.output.stream_eq(last)
 
         return m
-    
+
 class StreamRegister(Elaboratable):
     def __init__(self, shape):
         self.input  = SampleStream(shape)
